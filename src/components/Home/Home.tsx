430a6b8433abb44e84089682d215d7d3f62cf8bd
seyha26 <seyhachheng26@email.com>
2023-11-20 17:03:38 +0700

    initial

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,10 +6,10 @@ const Home = () => {
     <section>
       <div className="home-wrapper bg-[#EBF3E8]">
         {/* <div className="hero flex max-w-[1300px] mx-auto px-[50px] h-screen items-center justify-center"> */}
-        <Contianer className="flex flex-1 justify-center items-center h-screen px-6 ">
-          <div className="flex flex-col gap-10 items-start text-3xl w-full font-[500]">
+        <Contianer className="flex flex-1 md:flex-row flex-col justify-center gap-20 md:gap-0 items-center h-[90vh] md:h-screen px-6 ">
+          <div className="flex flex-col gap-10 md:items-start items-center text-3xl w-full font-[500]">
             <h1 className="md:text-5xl font-semibold">Hi there!</h1>
-            <h1 className="md:text-5xl lg:text-6xl font-bold">
+            <h1 className="font-bold">
               I'M <span className="text-[#427D9D]">CHHENG SEYHA</span>
             </h1>
             <h2 className="text-xl">Front-End Developer</h2>
@@ -17,7 +17,7 @@ const Home = () => {
               Download Resume
             </button>
           </div>
-          <div className="flex items-center w-full justify-end">
+          <div className="flex items-center w-full justify-center md:justify-end">
             <img
               className="w-[250px] h-[250px] md:h-[400px] md:w-[400px]"
               src={image}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import List from "./List";
 import { useEffect, useState } from "react";
 const Navbar = () => {
   const [isScrolling, setScrolling] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const scroll = () => {
@@ -19,6 +20,10 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", scroll);
   }, []);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   console.log(isScrolling);
 
   return (
@@ -27,13 +32,17 @@ const Navbar = () => {
         isScrolling ? "shadow-sm" : "shadow-none"
       }`}
     >
-      <nav className="max-w-[1300px] flex md:px-5 px-4 mx-auto items-center justify-between h-[100px]">
+      <nav className="max-w-[1300px] flex md:px-5 px-4 mx-auto items-center justify-between h-[70px] md:h-[100px]">
         <div>
           <Link className="text-3xl font-bold text-white uppercase" to="/">
             Logo
           </Link>
         </div>
-        <ul className="md:flex items-center flex-col hidden md:flex-row md:static w-full md:w-auto sm:absolute top-24 py-5   right-3 gap-10 rounded-sm">
+        <ul
+          className={`md:flex items-center flex-col  md:flex-row md:static w-full md:w-auto sm:absolute top-24 py-5   right-3 gap-10 rounded-sm ${
+            isOpen ? "flex top-0" : "hidden"
+          }`}
+        >
           <List
             className="text-white text-md md:text-lg"
             link=""
@@ -55,7 +64,9 @@ const Navbar = () => {
             title="Resume"
           />
         </ul>{" "}
-        <span className="md:hidden">Menu</span>
+        <span className="md:hidden" onClick={toggleMenu}>
+          Menu
+        </span>
       </nav>
     </header>
   );
